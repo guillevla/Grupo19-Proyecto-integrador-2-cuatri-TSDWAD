f1519ae3891467286e248059f03d1d4bfda91dc2
DiegoGuardatti <dguardatti@gmail.com>
2024-03-02 20:02:20 -0300

    mejoras de navegabilidad en los templates

--- Frontend/Templates/funcionesloginregistro.js
+++ Frontend/Templates/funcionesloginregistro.js
@@ -1,31 +1,6 @@
 // Variable global para almacenar usuarios registrados
 var usuarios = [];
 console.log('Usuarios almacenados:', usuarios);
-// Función de registro de nuevo usuario
-    document.addEventListener('DOMContentLoaded', function () {
-        var myModal = new bootstrap.Modal(document.getElementById('registroModal'), {
-            keyboard: false
-        });
-
-        document.getElementById('registroForm').addEventListener('submit', function (event) {
-            event.preventDefault();
-            var nombre = document.getElementById('nombre').value;
-            var email = document.getElementById('email').value;
-            var password = document.getElementById('passwordregistro').value;
-
-            // Almacena los datos en el array global de usuarios
-            usuarios.push({ username: nombre, email: email, password: password });
-            
-            // Puedes hacer más cosas aquí, como limpiar el formulario o mostrar un mensaje de éxito
-
-            // Cierra el modal
-            myModal.hide();
-        });
-
-        // Activa el modal cuando la página está completamente cargada
-        myModal.show();
-    });
-
 
 // Función de inicio de sesión de usuario
 function login() {
@@ -36,12 +11,43 @@ function login() {
     var user = usuarios.find(user => user.username === username && user.password === password);
 
     if (user) {
-        document.getElementById('error-message').innerText = '¡Inicio de sesión exitoso!';
+        document.getElementById('login-message').innerText = '¡Inicio de sesión exitoso!';
         // Puedes hacer más acciones después del inicio de sesión
         window.location.href = 'dashboard.html';
     } else {
-        document.getElementById('error-message').innerText = 'Credenciales incorrectas. Inténtalo de nuevo.';
+        document.getElementById('login-message').innerText = 'Credenciales incorrectas. Inténtalo de nuevo.';
     }
 }
 
+// Función de registro de nuevo usuario
+document.addEventListener('DOMContentLoaded', function () {
+    var myModal = new bootstrap.Modal(document.getElementById('registroModal'), {
+        keyboard: false
+    });
+
+    document.getElementById('registroForm').addEventListener('submit', function (event) {
+        event.preventDefault();
+        var nombre = document.getElementById('nombre').value;
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('passwordregistro').value;
+
+        // Almacena los datos en el array global de usuarios
+        usuarios.push({ username: nombre, email: email, password: password });
+        
+        document.getElementById('success-message').innerText = 'El usuario ' + nombre + ' se registró con éxito';
+        setTimeout(function () {
+            myModal.hide();
+            // Limpia el formulario
+            registroForm.reset();
+            // Limpia el mensaje después de ocultar el modal
+            document.getElementById('success-message').innerText = '';
+            window.location.href = 'dashboard.html';
+        }, 1000);
+        // Cierra el modal
+        //myModal.hide();
+        //registroForm.reset();
+        
+    });
+
+});
 
